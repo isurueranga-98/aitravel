2181e8409434035326c86b2c3580d4fc4cb9a1b1
Isuru Eranga <isurueranga.0@gmail.com>
2024-07-20 07:46:29 +0530

    font

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,14 @@
+import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
+
+  useFonts({
+    'outfit':require('./../assets/fonts/Outfit-Regular.ttf'),
+    'outfit-Bold':require('./../assets/fonts/Outfit-Bold.ttf'),
+    'outfit-Medium':require('./../assets/fonts/Outfit-Medium.ttf')
+  });
+  
   return (
     <Stack>
       <Stack.Screen name="index" />
