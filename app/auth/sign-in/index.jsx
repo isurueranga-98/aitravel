d70ff42815a7b3671ca55b1ac194a7ec29b3e09c
Isuru Eranga <isurueranga.0@gmail.com>
2024-07-20 09:34:04 +0530

    input

--- app/auth/sign-in/index.jsx
+++ app/auth/sign-in/index.jsx
@@ -1,10 +1,60 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, TextInput } from 'react-native'
+import React, { useEffect } from 'react'
+import {useNavigation} from 'expo-router'
+import { Colors } from '@/constants/Colors'
 
 export default function SignIn() {
+  const navigation = useNavigation();
+
+  useEffect(()=>{
+    navigation.setOptions({
+      headerShown:false
+    })
+  })
   return (
-    <View>
-      <Text>SignIn</Text>
+    <View style={{
+      padding:20,
+      paddingTop:50,
+      backgroundColor:Colors.WHITE,
+      height:'100%'
+    }}>
+      <Text style={{
+        fontFamily:'outfit-Bold',
+        fontSize:30
+      }}>Let's Sign You In</Text>
+
+
+      <Text style={{
+        fontFamily:'outfit',
+        fontSize:30,
+        color:Colors.GRAY,
+        marginTop:20
+      }}>Welcome Back</Text>
+
+
+      <Text style={{
+        fontFamily:'outfit',
+        fontSize:30,
+        color:Colors.GRAY,
+        marginTop:20
+      }}>Yoy have been missed</Text>
+
+      <View>
+        <Text style={{
+          fontFamily:'outfit',
+          fontSize:15,
+          color:Colors.GRAY,
+          marginTop:20
+        }}>Email</Text>
+        <TextInput
+          style={{
+            borderBottomColor:Colors.GRAY,
+            borderBottomWidth:1,
+            padding:10,
+            fontSize:20
+          }}
+        />
+      </View>
     </View>
   )
 }
--- components/Login.jsx
+++ components/Login.jsx
@@ -43,7 +43,7 @@ export default function Login() {
                 textAlign:'center',
                 fontFamily:'outfit-Bold'
               }}
-            >Login with Google</Text>
+            >Let's Start</Text>
           </TouchableOpacity>
       </View>
 
